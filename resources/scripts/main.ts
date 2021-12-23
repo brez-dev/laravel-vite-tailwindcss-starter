2635105cc27417b9c54af3e1a195133c0753ab5d
arikong <rahwinia51@gmail.com>
2021-12-23 10:48:34 +0900

    add innocenzi/laravel-vite

--- resources/scripts/main.ts
+++ resources/scripts/main.ts
@@ -3,13 +3,13 @@
 | Main entry point
 |--------------------------------------------------------------------------
 | Files in the "resources/scripts" directory are considered entrypoints
-| by default. 
-| 
+| by default.
+|
 | -> https://vitejs.dev
 | -> https://github.com/innocenzi/laravel-vite
 */
 
 import { createApp } from 'vue'
-import App from '@/views/App.vue'
+import App from '@/App.vue'
 
 createApp(App).mount('#app')
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
+require __DIR__ . '/auth.php';
 
-require __DIR__.'/auth.php';
+Route::get('{path}', function () {
+    return view('app');
+})->where('path', '([A-z\d\-\/_.]+)?');
