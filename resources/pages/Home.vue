82dcb3ca1351e89060c56ba0300c102d20fbb08c
arikong <rahwinia51@gmail.com>
2021-12-24 02:58:19 +0900

    tag fix

--- resources/pages/Home.vue
+++ resources/pages/Home.vue
@@ -8,7 +8,6 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
-
 export default defineComponent({
   name: 'Home',
   components: {
