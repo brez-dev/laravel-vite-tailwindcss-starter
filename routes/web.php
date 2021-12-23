85fd90579d624350bcb35de88648c83b3cbc33d4
arikong <rahwinia51@gmail.com>
2021-12-23 10:31:43 +0900

    add breeze api

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('app');
+    return ['Laravel' => app()->version()];
 });
+
+require __DIR__.'/auth.php';
