26496ed3de3ffc843f05ced2adf075eeb945905f
arikong <rahwinia51@gmail.com>
2021-12-28 20:57:18 +0900

    import tailwind css to main.ts

--- resources/scripts/main.ts
+++ resources/scripts/main.ts
@@ -3,4 +3,7 @@ import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
 
+// tailwindcss
+import '@/assets/css/tailwind.css'
+
 createApp(App).use(store).use(router).mount('#app')
